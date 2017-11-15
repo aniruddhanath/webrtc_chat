70046451e973da722866890e47455c9b42ef0534
Aniruddha Nath <aniruddhanath1@gmail.com>
2017-11-15 22:56:47 +0530

    unnecessary files removed

--- client/src/js/lib/Utils.js
+++
@@ -1,9 +0,0 @@
-import moment from "moment";
-
-export function generateToken() {
-  return Math.floor((1 + Math.random()) * 1e16).toString(16).substring(1);
-}
-
-export function formatTime(timestamp) {
-  return moment(timestamp).fromNow(true);
-}
